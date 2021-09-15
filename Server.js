4df1dc31e51f0a879e95968e493ad231306d75a1
wonkooklee <const.wonkook@gmail.com>
2021-09-15 16:38:55 +0900

    Fixed: 초기 세팅 수정 반영

--- Server.js
+++ Server.js
@@ -1,16 +1,17 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 const PORT = 8000 | process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hello");
+app.get('/', (req, res) => {
+  res.send('Hello');
 });
 
-app.listen(PORT, (err) => {
-  if (!err) {
+app.listen(PORT, err => {
+  try {
     console.log(`Server is running on ${PORT}`);
-  } else {
-    console.log(err);
+    throw new Error('Server is not ok');
+  } catch (e) {
+    console.error(e);
   }
 });
