4d584296273650eadc48a092bf1b0ab588b6d20d
minjaekim <minjae2246@gmail.com>
2021-09-28 14:07:29 +0900

    Modified: 주석 삭제

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const routes = require("./routes");
-// const looger = require("morgan")("dev")
 
-// const cors = require("cors");
 const app = express();
 
 app.use(express.json());
-// app.use(cors());
 app.use(routes);
 
 app.use((err, req, res, next) => {
