88cb2a68726f21f19e0a9f5b54578294b6f95300
jin0oon <86861297+jin0oon@users.noreply.github.com>
2021-09-29 16:14:44 +0900

    Fix | 메모 파일 삭제 (주석은 삭제 안 함)

--- middlewares/checkAuth.js
+++ middlewares/checkAuth.js
@@ -12,4 +12,22 @@ const checkAuth = (req, res, next) => {
   next();
 }
 
+// import jwt from "jsonwebtoken"
+
+// const checkAuth = (req, res, next) => {
+//   const token = req.header.token
+//   if(!token) {
+//     new Error('INVALID USER', 401)
+//   }
+
+//   let resolvedToken = jwt.verify
+//   /*
+//   let resolvedToken = jwt.verify(token, 'secret_key')
+
+//   userId = {resolvedToken.userId}
+//   req.user = userId //이러면 새 속성이 생김 (?) //없는 키의 프로퍼티를 나중에 추가한단 얘긴 아닌듯.
+//   */
+//   next();
+// }
+
 export default checkAuth;
